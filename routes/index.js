7119eba0d6d47b361b0b3709e0a5e9317c88a045
vishnupriyats <vishnupriya0913@gmail.com>
2020-04-24 11:43:07 +0000

    flash changes commit

--- routes/index.js
+++ routes/index.js
@@ -26,7 +26,7 @@ router.post("/register", function(req, res){
             return res.render("register");
         }
         passport.authenticate("local")(req, res, function(){
-			req.flash("success", "Welcome to YelpCamp " + user.username);
+			req.flash("success", "Welcome to MovieBuzz " + user.username);
            res.redirect("/movies"); 
         });
     });
